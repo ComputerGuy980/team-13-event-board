287f7c8b418201ff74b8e78a9ff639c0ffe5705d
ComputerGuy980 <computerguy980@gmail.com>
2026-04-15 11:36:10 -0400

    feat: event repository interface and first impl.

---
+++ src/event/InMemoryEventRepository.ts
@@ -0,0 +1,54 @@
+import { match } from "node:assert";
+import { type AuthError, UnexpectedDependencyError } from "../auth/errors";
+import { Err, Ok, type Result } from "../lib/result";
+import type { EventStatus, IEventRecord } from "./Event";
+import type { IEventRepository } from "./EventRepository";
+
+class InMemoryEventRepository implements IEventRepository {
+    constructor(private readonly events: IEventRecord[]) {}
+
+    async create_event(event: IEventRecord): Promise<Result<IEventRecord | null, AuthError>> {
+        try {
+            this.events.push(event);
+            return Ok(event);
+        } catch {
+            return Err(UnexpectedDependencyError("Failed to create event."));
+        }
+    }
+
+    async get_event(id: number): Promise<Result<IEventRecord | null, AuthError>> {
+        try {
+            const match = this.events.find(e => e.id === id);
+            if (!match) return Err(UnexpectedDependencyError(`Cannot find event with id ${id}.`));
+            return Ok(match);
+        } catch {
+            return Err(UnexpectedDependencyError("Failed to get event."))
+        }
+    }
+
+    async edit_event(id: number, event: IEventRecord): Promise<Result<boolean, AuthError>> {
+        try {
+            const match_idx = this.events.findIndex(e => e.id === id);
+            if (match_idx < 0) return Err(UnexpectedDependencyError(`Cannot find event with id ${id}.`));
+            this.events[match_idx] = event;
+            return Ok(true);
+        } catch {
+            return Err(UnexpectedDependencyError("Failed to edit event."))
+        }
+    }
+
+    async set_event_status(id: number, status: EventStatus): Promise<Result<boolean, AuthError>> {
+        try {
+            const match = this.events.find(e => e.id === id);
+            if (!match) return Err(UnexpectedDependencyError(`Cannot find event with id ${id}.`));
+            match.status = status;
+            return Ok(true);
+        } catch {
+            return Err(UnexpectedDependencyError("Failed to change event status."));
+        }
+    }
+
+    async list_events(): Promise<Result<IEventRecord[], AuthError>> {
+        return Ok(this.events);
+    }
+}
